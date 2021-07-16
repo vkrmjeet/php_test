1ae2576b024720cb7c83ec01b9f63b4ef65d48b3
vkrmjeet <67248881+vkrmjeet@users.noreply.github.com>
2021-07-16 17:23:25 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
 <html>
+  <head><title>PHP test page </title></head>
 <body>
 
 <?php
 $txt = "PHP";
 echo "I love $txt!";
+  $php="good language"
+    echo "$txt is a $php";
 ?>
 
 </body>
